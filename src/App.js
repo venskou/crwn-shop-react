5f88f629f50d31a664401a323f79600ce20fc62d
Uladzislau Vensko <ulad.vensko@gmail.com>
2019-12-19 19:51:10 +0300

    Implement shop page with collection components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-import './App.css'
+import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
-    return <HomePage />;
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+        </Switch>
+      </div>
+    )
   }
 }
 
