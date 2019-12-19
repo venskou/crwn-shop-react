a96033e682b4f6f53f1014a8664b982c33412142
Uladzislau Vensko <ulad.vensko@gmail.com>
2019-12-19 15:13:50 +0300

    Implement homepage directory

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import HomePage from './pages/homepage/homepage.component';
+
+import './App.css'
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return <HomePage />;
   }
 }
 
