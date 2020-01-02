fb1c2fd7c924c7a128098d72cf3de628af79069c
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-02 13:19:17 +0300

    Remove redux-logger, add redux browser etension settings

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
-
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export const persistor = persistStore(store);
