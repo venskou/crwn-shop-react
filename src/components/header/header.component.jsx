17b7ee9411c4ce9829d2702342d91c82a5eeb4ca
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-06 08:46:18 +0300

    Fix sign out link

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -20,7 +20,7 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/contact">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionLink as="div" onClick={() => auth.signOut()}>
+        <OptionLink as="div" onClick={() => auth.signOut()} to={''}>
           SIGN OUT
         </OptionLink>
       ) : (
