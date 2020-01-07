b5ba74e05ee0c9805fc6102a0805cdc638d68d2e
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-07 16:48:18 +0300

    Include redux-thunk as middleware and refactor shop actions and reducers to work with redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-
+import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
 
-export const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [thunk];
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 export const persistor = persistStore(store);
