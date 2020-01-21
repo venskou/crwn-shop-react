786fbe265d626062e7cf7f4c8930f98ab2dce835
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-21 08:14:02 +0300

    Remove index.css styles from index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
