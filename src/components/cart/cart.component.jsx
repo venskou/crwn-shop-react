94ea472a4bd7c79e7c5ba9d7f8c90e2e6837ee40
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-23 14:16:46 +0300

    Add cart component responsive styles and logic

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -1,33 +1,41 @@
 import React, { useRef } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { useMediaQuery } from 'react-responsive';
+
 import {
   selectCartItems,
   selectCartItemsCount,
-  selectCartHidden,
+  selectCartVisibility,
 } from '../../redux/cart/cart.selectors';
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { setCartVisibility } from '../../redux/cart/cart.actions';
+import { setOverlayVisibility } from '../../redux/overlay/overlay.actions';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './cart.styles.scss';
 
-const Cart = ({ itemCount, hidden, toggleCartHidden }) => {
+const Cart = ({ itemCount, setCartVisibility, isCartVisible, setOverlayVisibility }) => {
   const cartToggle = useRef(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 576 });
+
+  const toggleCart = () => {
+    setCartVisibility(!isCartVisible);
+    if (isMobile) {
+      setOverlayVisibility(!isCartVisible);
+    }
+  };
 
   return (
     <div className="cart">
-      <button
-        className="cart__dropdown-toggle"
-        type="button"
-        ref={cartToggle}
-        onClick={toggleCartHidden}
-      >
+      <button className="cart__dropdown-toggle" type="button" ref={cartToggle} onClick={toggleCart}>
         <ShoppingIcon />
         <span className="cart__item-count">{itemCount}</span>
       </button>
-      {hidden && <CartDropdown cartToggleRef={cartToggle} />}
+      <CartDropdown cartToggleRef={cartToggle} />
     </div>
   );
 };
@@ -35,11 +43,12 @@ const Cart = ({ itemCount, hidden, toggleCartHidden }) => {
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
   itemCount: selectCartItemsCount,
-  hidden: selectCartHidden,
+  isCartVisible: selectCartVisibility,
 });
 
 const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()),
+  setCartVisibility: isVisible => dispatch(setCartVisibility(isVisible)),
+  setOverlayVisibility: isVisible => dispatch(setOverlayVisibility(isVisible)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
