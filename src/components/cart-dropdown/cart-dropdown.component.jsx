4b2fd4e9ac288d3a5c87b25453bd3f80476324da
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-23 14:58:37 +0300

    Card dropdown component responsive styles and logic

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,19 +2,45 @@ import React, { useRef, useEffect } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
+import { useMediaQuery } from 'react-responsive';
+import classNames from 'classnames';
 
-import { selectCartItems } from '../../redux/cart/cart.selectors';
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItems, selectCartVisibility } from '../../redux/cart/cart.selectors';
+
+import { setCartVisibility } from '../../redux/cart/cart.actions';
+import { setOverlayVisibility } from '../../redux/overlay/overlay.actions';
 
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { createStructuredSelector } from 'reselect';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ history, cartItems, toggleCartHidden, cartToggleRef }) => {
+const CartDropdown = ({
+  cartToggleRef,
+  isCartVisible,
+  setCartVisibility,
+  setOverlayVisibility,
+  history,
+  cartItems,
+}) => {
   const cartDropdown = useRef(null);
   const cartToggle = cartToggleRef;
+
+  useMediaQuery({ maxWidth: 576 }, undefined, isMobile => {
+    if (isMobile && isCartVisible) {
+      setOverlayVisibility(true);
+    }
+
+    if (!isMobile && isCartVisible) {
+      setOverlayVisibility(false);
+    }
+  });
+
+  const hideCart = () => {
+    setCartVisibility(false);
+    setOverlayVisibility(false);
+  };
 
   useEffect(() => {
     const listener = ({ target }) => {
@@ -28,18 +54,31 @@ const CartDropdown = ({ history, cartItems, toggleCartHidden, cartToggleRef }) =
         return;
       }
 
-      toggleCartHidden();
+      hideCart();
     };
 
-    document.addEventListener('mousedown', listener);
+    if (isCartVisible) {
+      document.addEventListener('mousedown', listener);
+    } else {
+      document.removeEventListener('mousedown', listener);
+    }
 
     return () => {
       document.removeEventListener('mousedown', listener);
     };
-  }, [cartToggle, cartDropdown]);
+  }, [cartToggle, cartDropdown, isCartVisible]);
+
+  const cartClass = classNames('cart-dropdown', {
+    'cart-dropdown--open': isCartVisible,
+  });
+
+  const goToCheckout = () => {
+    hideCart();
+    history.push('/checkout');
+  };
 
   return (
-    <div className="cart-dropdown" ref={cartDropdown}>
+    <div className={cartClass} ref={cartDropdown}>
       <div className="cart-dropdown__items">
         {cartItems.length ? (
           cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
@@ -47,14 +86,7 @@ const CartDropdown = ({ history, cartItems, toggleCartHidden, cartToggleRef }) =
           <p className="cart-dropdown__empty-message">Your cart is empty</p>
         )}
       </div>
-      <CustomButton
-        onClick={() => {
-          toggleCartHidden();
-          history.push('/checkout');
-        }}
-        className="cart-dropdown__checkout-btn"
-        type="button"
-      >
+      <CustomButton onClick={goToCheckout} className="cart-dropdown__checkout-btn" type="button">
         GO TO CHECKOUT
       </CustomButton>
     </div>
@@ -63,10 +95,12 @@ const CartDropdown = ({ history, cartItems, toggleCartHidden, cartToggleRef }) =
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
+  isCartVisible: selectCartVisibility,
 });
 
 const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()),
+  setCartVisibility: isVisible => dispatch(setCartVisibility(isVisible)),
+  setOverlayVisibility: isVisible => dispatch(setOverlayVisibility(isVisible)),
 });
 
 export default compose(connect(mapStateToProps, mapDispatchToProps), withRouter)(CartDropdown);
