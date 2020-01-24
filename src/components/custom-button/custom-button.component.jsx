a55d8922ba490c08874efbe76a2af09911133b80
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-24 20:17:28 +0300

    Refactor custom-button component styles

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
 
-import { CustomButtonContainer } from './custom-button.styles';
+import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
-);
+const CustomButton = ({ children, className, type }) => {
+  const blockClass = 'custom-button';
+  const customButtonClass = classnames(className, blockClass, {
+    [`${blockClass}--${type}`]: type === 'inverse',
+    [`${blockClass}--${type}`]: type === 'blue',
+  });
+
+  return <button className={customButtonClass}>{children}</button>;
+};
 
 export default CustomButton;
