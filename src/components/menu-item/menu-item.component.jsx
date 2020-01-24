4696f22b7a8687ce5df1e0e4b0cb06c788574313
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-24 09:02:34 +0300

    Refactor menu-item component markup, styles and directory styles

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import classNames from 'classnames';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
-);
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  const menuItemClass = classNames('menu-item', {
+    [`menu-item--${size}`]: size,
+  });
+
+  return (
+    <Link className={menuItemClass} to={`${match.url}${linkUrl}`}>
+      <img className="menu-item__cover" src={imageUrl} alt={title} />
+      <div className="menu-item__content">
+        <h2 className="menu-item__title">{title}</h2>
+        <p className="menu-item__subtitle">Shop Now</p>
+      </div>
+    </Link>
+  );
+};
 
 export default withRouter(MenuItem);
