a8607b8859daeaf9b52c678d816b20d21b2625c4
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-25 09:40:01 +0300

    Fix custom-button class names generation and add uppercase to styles

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import classnames from 'classnames';
+import classNames from 'classnames';
 
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, className, type }) => {
   const blockClass = 'custom-button';
-  const customButtonClass = classnames(className, blockClass, {
-    [`${blockClass}--${type}`]: type === 'inverse',
-    [`${blockClass}--${type}`]: type === 'blue',
+
+  const customButtonClass = classNames(className, blockClass, {
+    [`${blockClass}--inverse`]: type === 'inverse',
+    [`${blockClass}--blue`]: type === 'blue',
   });
 
   return <button className={customButtonClass}>{children}</button>;
