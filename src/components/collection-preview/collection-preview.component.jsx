259884f5196adb8a01ea80eafafd6b72cfc72816
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-25 11:20:54 +0300

    Refactor collection-preview component, make title as link in collection page and refactor styles

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title}</h1>
-    <div className="preview">
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </div>
-  </div>
-);
+const CollectionPreview = ({ title, items, routeName }) => {
+  const { path } = useRouteMatch();
+
+  return (
+    <section className="collection-preview">
+      <h2 className="collection-preview__title">
+        <Link to={`${path}/${routeName}`}>{title}</Link>
+      </h2>
+      <div className="collection-preview__items">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </section>
+  );
+};
 
 export default CollectionPreview;
