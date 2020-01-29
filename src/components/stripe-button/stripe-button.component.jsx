ec8ba6e00a500f29ceba8cb2622a00152ddfb472
Uladzislau Vensko <ulad.vensko@gmail.com>
2020-01-29 11:27:24 +0300

    Cleart cart after successful payment

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
 
-const StripeCheckoutButton = ({ price }) => {
+import { clearCart } from '../../redux/cart/cart.actions';
+
+const StripeCheckoutButton = ({ price, clearCart }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_0lrDq0TJBXTGnn9hfJ3kRu5i00qkxd68md';
 
   const onToken = token => {
-    console.log(token);
     alert('Payment Successful');
+    clearCart();
   };
 
   return (
@@ -26,4 +29,8 @@ const StripeCheckoutButton = ({ price }) => {
   );
 };
 
-export default StripeCheckoutButton;
+const mapDispatchToProps = dispatch => ({
+  clearCart: () => dispatch(clearCart()),
+});
+
+export default connect(null, mapDispatchToProps)(StripeCheckoutButton);
